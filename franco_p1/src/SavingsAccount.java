3189289d2e4ad0a3a5c187350433a6f83629d922
Shelby Franco <sfranco@knights.ucf.edu>
2019-10-18 03:11:06 -0400

    problem 2 commit

---
+++ franco_p1/src/SavingsAccount.java
@@ -0,0 +1,28 @@
+
+public class SavingsAccount {
+    private static double savingsBalance;
+    private static double annualInterestRate;
+
+    public static void calculateMonthlyInterest() {
+        double monthlyInterest;
+        monthlyInterest = ((savingsBalance * annualInterestRate) / 12);
+        savingsBalance += monthlyInterest;
+    }
+
+    public static double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public static void modifyInterestRate (double newInterestRate){
+        annualInterestRate = newInterestRate;
+    }
+    public static void setBalance(double userBalance) {
+        savingsBalance = userBalance;
+
+    }
+    public static void printBalance() {
+        System.out.printf("Your Balance is $%.2f\n", savingsBalance);
+    }
+
+
+}
